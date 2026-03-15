35f39293fc96ac4998f5ebec3898e5fdecacbfe8
LASC-dev <lucas.a.s.casas@outlook.es>
2026-03-15 15:01:24 -0300

    refactor: allow moving any file type and handle directory destinations

--- app/main.py
+++ app/main.py
@@ -5,27 +5,27 @@ def move_file(
         command: str
 ) -> None:
     """
-    Moves a text file from one location to another based on a command.
+    Moves a file from one location to another based on a command.
 
-    Takes a string in the format 'mv source destination.txt', creates the
-        necessary folders if they don't exist, copies the contents, and
-        deletes the original.
+    Takes a string in the format 'mv source destination', creates the
+    necessary folders if they don't exist, copies the contents, and
+    deletes the original. Handles cases where the destination is a directory.
 
     Args:
-
-        command: A string containing the move instruction.
-            Must follow the format "mv <source_file> <destination_file.txt>".
+        command: A string containing the move instruction with
+        the format: "mv <source_file> <destination_path>".
     """
     cmd_parts = command.split()
 
     if len(cmd_parts) == 3 and cmd_parts[0] == "mv":
-        source = cmd_parts[1]
-        dest = cmd_parts[2]
-        if os.path.exists(source) and dest.endswith(".txt"):
+        _, source, dest = cmd_parts
+        if os.path.exists(source):
+            if dest.endswith("/"):
+                dest = os.path.join(dest, os.path.basename(source))
             dest_dir = os.path.dirname(dest)
             if dest_dir and not os.path.exists(dest_dir):
                 os.makedirs(dest_dir)
-            with open(source) as origin, open(dest, "w") as copy:
+            with open(source, "rb") as origin, open(dest, "wb") as copy:
                 for line in origin:
                     copy.write(line)
             os.remove(source)
